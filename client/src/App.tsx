82ff8142c0d1f151c12c909c0042dc9127be8d25
Aljamil1113 <aarazasaljamil@gmail.com>
2022-12-29 18:47:20 +0800

    API and CLient: Cors and Fetch data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [products, setProducts] = useState([
+    {name: 'product1', price: 100.00}
+  ])
+
+  useEffect(() => {
+    fetch('https://localhost:7280/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Re-Store</h1>
+
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
     </div>
   );
 }
