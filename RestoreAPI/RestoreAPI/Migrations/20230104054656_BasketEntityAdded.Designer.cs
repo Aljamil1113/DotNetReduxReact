0bfa36b0d8f426debd11256f0054e463921a5af3
Aljamil1113 <aarazasaljamil@gmail.com>
2023-01-06 23:51:56 +0800

    API: Shopping Cart Feauture

--- RestoreAPI/RestoreAPI/Migrations/20230104054656_BasketEntityAdded.Designer.cs
+++ RestoreAPI/RestoreAPI/Migrations/20230104054656_BasketEntityAdded.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestoreAPI.Data;
 
@@ -10,9 +11,10 @@ using RestoreAPI.Data;
 namespace RestoreAPI.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20230104054656_BasketEntityAdded")]
+    partial class BasketEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
